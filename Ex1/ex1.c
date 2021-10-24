4088f6ded8b7bbc75c9bc1a0307f177535e8d6bc
IdanZiv97 <idanziv7@gmail.com>
2021-10-24 12:11:17 +0300

    Initial try for task 3 - did not work

--- Ex1/ex1.c
+++ Ex1/ex1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "ex1.h"
-#define WORD_SIZE 8;
+#define BYTE_SIZE 8;
 typedef unsigned char *byte_pointer;
 unsigned long merge_bytes_bigAid(unsigned long x, unsigned long int y);
 unsigned long merge_bytes_littleAid(unsigned long x, unsigned long int y);
+unsigned long put_byte_aidLittle(unsigned long x, unsigned char b, int i);
+/* Task 1 */
 /**
  * We initilize an int with the value of 1.
  * We know that it is stored in the memory in a 4-byte adress.
@@ -26,6 +28,7 @@ int is_big_endian() {
         return 1;
     }   
 }
+/* Task 2 */
 
 unsigned long merge_bytes(unsigned long x, unsigned long int y) {
     unsigned long word;
@@ -40,7 +43,6 @@ unsigned long merge_bytes(unsigned long x, unsigned long int y) {
 /**
  * This function will be invoked when we 
  * */
-
 unsigned long merge_bytes_bigAid(unsigned long x, unsigned long int y) {
     return 1;
 }
@@ -60,4 +62,28 @@ unsigned long merge_bytes_littleAid(unsigned long x, unsigned long int y) {
     y = y & maskY;
     x = x & maskX;
     return (x | y);
+}
+
+/* Task 3 */
+/**
+ * This function writes a given char b into an existing word.
+ * The procedure will be done using bit masking and switching.
+ * First we will turn to '0' the bits to be replaced by the char.
+ * Then we will create a a mask from the char given and merge the two with
+ * OR ('|').
+ **/
+unsigned long put_byte(unsigned long x, unsigned char b, int i) {
+    if (is_big_endian()) {
+        return 0;
+    } else {
+       return put_byte_aidLittle(x, b, i);
+    }
+}
+
+unsigned long put_byte_aidLittle(unsigned long x, unsigned char b, int i) {
+    int shiftIndex = i * BYTE_SIZE;
+    unsigned long mask = 0xff << shiftIndex;
+    x = x & ~mask;
+    unsigned long newByte = b << shiftIndex;
+    return (x | newByte);
 }
