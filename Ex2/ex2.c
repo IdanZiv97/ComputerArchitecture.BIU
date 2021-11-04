0f93d572e7be257f3aeb63b220593a82f43e67eb
IdanZiv97 <idanziv7@gmail.com>
2021-11-04 12:26:54 +0200

    equal() defenition

--- Ex2/ex2.c
+++ Ex2/ex2.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <assert.h>
 #include "ex2.h"
 #define MSB_MASK 0x80000000
 #define ZERO_SIGNED MSB_MASK
 #define BYTE 8;
 
+int isZero(magnitude m);
+/**
+ * isEqual fucntion comapres the bits of the magnitudes to check if there is at least one bit who is diferent.
+ * The functions also takes into consideration the two different representation of zero.
+ * The ^ operator in case of equal mangitudes will result in 0, otherwise at least one pair of bits are diferent which will result
+ * in the result proper bit to be set to 1, which will create a value diferent from 0.
+ **/
 int equal(magnitude a, magnitude b) {
     int isEqual = 1; //indicates true
     //check for both representation of zero
@@ -11,13 +19,10 @@ int equal(magnitude a, magnitude b) {
         return isEqual;
     }
     //check for mis-matching MSB
-    if(a & MSB_MASK != b & MSB_MASK) {
+    if((a & MSB_MASK) != (b & MSB_MASK)) {
         return isEqual = 0;
     }
-    //using the ^ operator means that for every eqaul bit the operator
-    //will turn it to 0 - if they are equal so are all their bits and
-    //the result should be 0, if there's at least one pair of bits which is different 
-    //the result will different than 
+    //check if for a pair of diferent bits
     if (a ^ b) {
         return isEqual = 0;
     } else {
@@ -25,12 +30,16 @@ int equal(magnitude a, magnitude b) {
     }
 }
 
+//need to fix this bug
 int isZero(magnitude m) {
     //indicates m is a zero
     int isZero = 1;
-    if (m & 0x0 || m & ZERO_SIGNED) {
+    //If it is actually 0.
+    if (m == 0) {
         return isZero;
-    } else {
+    } else if (m ^ ZERO_SIGNED) {
         return isZero = 0;
+    } else {
+       return isZero;
     }
 }
