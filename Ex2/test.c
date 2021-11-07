39cde87d511add4877edc037682b8adf3806122e
IdanZiv97 <idanziv7@gmail.com>
2021-11-07 13:10:32 +0200

    Tests for add function - case: both numbers are positive

--- Ex2/test.c
+++ Ex2/test.c
@@ -3,7 +3,6 @@
 #include "ex2.h"
 #define MSB_MASK 0x80000000
 #define ABS_MASK 0x7fffffff
-
 int printMagnitude(magnitude m);
 
 /**
@@ -25,17 +24,28 @@ int printMagnitude(magnitude m) {
 
 int main()
 {
+    //testing add fucntion for positive numbers
     magnitude m1 = 0b0100; //=4
-    magnitude m2 = 0x80000004; //-4
-    magnitude m3 = 0x3BEB8;//=245432
-    magnitude m4 = 0x8003BEB8;//-245432
-    printf("m1 = %d\n", printMagnitude(m1));
-    printf("m2 = %d\n", printMagnitude(m2));
-    printf("m3 = %d\n", printMagnitude(m3));
-    printf("m4 = %d\n", printMagnitude(m4));
-    assert(printMagnitude(m1) == 4 ? printf("Passed test m1\n") : printf("m1: expected 4\n"));
-    assert(printMagnitude(m2) == -4 ? printf("Passed test m2\n") : printf("m2: expected -4\n"));
-    assert(printMagnitude(m3) == 245432 ? printf("passed test m3\n") : printf("m3: expected 245432\n"));
-    assert(printMagnitude(m4) == -245432 ? printf("passed test m4\n") : printf("m4: expected -245432\n"));
+    magnitude m2 = 0b1100; //=12
+    magnitude m3 = 0b11011;//=27
+    magnitude m4 = 0b11000101;//=197;
+    magnitude m5 = 0x80000000;//=0;
+    magnitude m6 = 0;//=0;
+    magnitude m7 = 0b00001;//=1;
+    magnitude m8 = 0b10001;//17
+    magnitude test1 = add(m1, m2);//=16
+    magnitude test2 = add(m3, m4);//=224
+    magnitude test3 = add(m3, m5);//=27
+    magnitude test4 = add(m7, m8);//=18
+    magnitude test5 = add(m6, m5);//=0
+    magnitude test6 = add(m8, m4);//=214
+    magnitude test7 = add(m8, m2);//=29
+    assert(printMagnitude(test1) == 16 ? printf("Passed test1\n") : printf("test1: expected 16 got %d\n", test1));
+    assert(printMagnitude(test2) == 224 ? printf("Passed test2\n") : printf("test2: expected 224 got %d\n", test2));
+    assert(printMagnitude(test3) == 27 ? printf("Passed test3\n") : printf("test3: expected 27 got %d\n", test3));
+    assert(printMagnitude(test4) == 18 ? printf("Passed test4\n") : printf("test4: expected 18 got %d\n", test4));
+    assert(printMagnitude(test5) == 0 ? printf("Passed test5\n") : printf("test5: expected 0 got %d\n", test5));
+    assert(printMagnitude(test6) == 214 ? printf("Passed test6\n") : printf("test6: expected 214 got %d\n", test6));
+    assert(printMagnitude(test7) == 29 ? printf("Passed test7\n") : printf("test7: expected 219 got %d\n", test7));
     return 0;
 }
