ee5be3c64828a74a96c739c358701f1f5c6dcc0e
idan.Ubunto <idanziv7@gmail.com>
2021-11-22 10:46:28 -0800

    Notes

--- Ex3/run_main.s
+++ Ex3/run_main.s
@@ -1,32 +1,20 @@
 #318175197 Idan Ziv
 .section    .rodata
-    .extern scanf
     .align 8
     #literals for scanf and printf
     format_int:     .string "%d"
     format_str:     .string "%s"
-    .text
-    .global run, main
-    .type run, @function
+.section    .text
+    .global run_main, main
+    .type run_main, @function
 main:
-    movq %rsp, %rbp #for correct debugging
-    call run
-run:
-    #intial stack frame for the procedure
+    call run_main
+run_main:
     pushq %rbp
     movq %rsp, %rbp
-    #make space for a pstring - lets check if i know how to read them
-    subq $500, %rsp #the size of a p string
-    #reading the size of the pstring
-    movq $format_int, %rdi #passing the data format for scanf 
-    leaq -500(%rbp), %rsi #passing the address to store the size
-    movq $0, %rax
+    subq $528, %rsp #create space for local pstrings
+    movq $format_int, %rdi #sending the format for the size of the 1st p string
+    leaq -528(%rbp), %rsi #sedning the addres to the size of the 1st pstring variable
+    xorq %rax, %rax #set %rax to 0
     call scanf
-    #reading the string
-    movq $format_str, %rdi
-    leaq -499(%rbp), %rsi
-    movq $0, %rax
-    call scanf
-    
- 
-    
+    ret
