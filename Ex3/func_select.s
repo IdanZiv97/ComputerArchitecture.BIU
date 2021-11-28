e888d1840c3ae763d65dd938c30cbfc01f6c4d1a
Idan Ziv <idanziv7@gmail.com>
2021-11-28 08:19:28 -0800

    Added pstrlen function calling

--- Ex3/func_select.s
+++ Ex3/func_select.s
@@ -1,8 +1,8 @@
-#318175197 Idan Ziv
-.section    .rodata
+# 318175197 Idan Ziv
+    .section    .rodata
+
     .align 8
-#cases for switch case
-.JUMP_TABLE:
+.JUMP_TABLE:    # cases for switch case
     .quad   .f_pstrlen #case 50
     .quad   .f_default #case 51 - default
     .quad   .f_replaceChar #case 52
@@ -15,23 +15,51 @@
     .quad   .f_default #case 59 - default
     .quad   .f_strlen #case 60
 
-#literals for pstr.h functions
-msg_pstrlen:     .string "first pstring length: %d,second pstring length: %d\n"
-msg_replaceChar:    .string "old char: %c, new char: %c, first string: %s, second string: %s\n"
-format_pstr_info:   .string "lenght: %d, string: %s\n"
-msg_pstrijcmp:  .string "comapre result: %d\n"
-msg_default_case:   .string "invalid option\n"
-#literals for scanf, printf
-format_int:     .string: "%d"
-format_char:    .string: "%c"
-format_str:     .string: "%s"
+    # literals for pstr.h functions
+    msg_pstrlen:     .string "first pstring length: %d,second pstring length: %d\n"
+    msg_replaceChar:    .string "old char: %c, new char: %c, first string: %s, second string: %s\n"
+    format_pstr_info:   .string "lenght: %d, string: %s\n"
+    msg_pstrijcmp:  .string "comapre result: %d\n"
+    msg_default_case:   .string "invalid option\n"
+    # literals for scanf, printf
+    format_scan_int:     .string "%d"
+    format_scan_char:    .string "%c"
+    format_scan_string:    .string "%s"
 
     .text
-.global run_func
-.type run_func, @function
-run_func: #the case number is in %rdi    
-    #correcting the input offset to match the first case
-    leaq -50(%rdi), %rbx #setting the choice to match the range
-    cmpq $10, %rbx #check if the choice is in range
-    ja .f_default #if the number is not in range we ha
+    .global run_func
+    .type run_func, @function
+run_func:   # the case number is in %rdi (%edi), the 1st pString in %rsi, the 2nd pString in %rdx
+    # correcting the input offset to match the first case
+    leaq -50(%rdi), %rbx # setting the choice to match the range
+    cmpq $10, %rbx # check if the choice is in range
+    ja .f_default # if the number is not in range we ha
     jmp *.JUMP_TABLE(,%rsi,8)
+
+.f_pstrlen:
+    # creating stack frame - saving the pString pointers
+    pushq    %rbp
+    movq    %rsp, %rbp
+    pushq    %rsi    # saving the 1st pString pointer
+    pushq    %rdx    # saving the 2nd pString poiner
+
+    # getting the length of the 1st pString
+    leaq    -8(%rbp), %rdi    # send the pointer as the parameter
+    call    pstrlen
+    movq    %rax, %rsi    # saving the return value in %rsi
+
+    # getting the length of the 2nd pString
+    leaq    -16(%rbp), %rdi    # send the pointer as the parameter
+    call    pstrlen
+    movq    %rax, %rdx    # saving the return value in %rdi
+
+    # print the message
+    movq    $msg_pstrlen, %rdi    # pasing the proper message to printf
+    call    printf    # Note: in %rsi and %rdx we saved the return values from calling pstrlen
+
+    # resotre the stack frame
+    popq    %rdx
+    popq    %rsi
+    popq    %rbp
+    movq    %rbp, %rsp
+    ret
