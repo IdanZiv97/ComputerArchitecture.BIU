e7756947589bf5db372e72df0d0131ac7db4ceb4
Idan Ziv <idanziv7@gmail.com>
2021-11-28 05:54:28 -0800

    trying to allign the stack

--- Ex3/run_main.s
+++ Ex3/run_main.s
@@ -1,32 +1,79 @@
-    #318175197 Idan Ziv
+    # 318175197 Idan Ziv check the tertain
     .section    .rodata
         .align 8
-        #literals for scanf and printf
-        format_int:     .string "%d"
-        format_str:     .string "%s"
+        # literals for scanf and printf
+        format_int:     .string "% d"
+        format_str:     .string "% s\0"
+        format_test1:    .string "pstr1: %d, %s\n"
+        format_test2:   .string "pstr2: %d, %s\n"
+        format_test3:   .string "choice: %d\n"
     .section    .text
         .global main
         .type main, @function
 main:
-    pushq %rbp
-    movq %rsp, %rbp
-    pushq %r12
-    pushq %r11 # pushq used to proper align the stack pointer adress
-    subq $512, %rsp # create space for local pstrings
-    movq $format_int, %rdi # sending the format for the size of the 1st p string
-    leaq -512(%rbp), %rsi # loading the address to the size of the 1st pstring variable
-    xorq %rax, %rax # set %rax to 0
+    movq %rsp, %rbp #for correct debugging
+    # initialize the stack frame
+    pushq   %rbp
+    movq    %rsp, %rbp
+    # pushq used to proper align the stack pointer adress
+    pushq   %r12
+    # pushq   %r11
+    # Creating the stack frame for the function
+    subq    $536, %rsp  # two pStrings (256 * 2 = 512) and 1 int for the choice (8 bytes for the address)
+
+    # 1st pString
+
+    # get the length
+    movq    $format_int, %rdi    # pass the 1st argument for scanf
+    leaq    -528(%rbp), %rsi    # pass the 2nd argument for scanf
+    xorq    %rax, %rax
+    call    scanf
+    # get the string
+    movq    $format_str, %rdi    # pass the 1st argument for scanf
+    leaq    -527(%rbp), %rsi    # pass the 2nd argument for scanf
+    xorq    %rax, %rax
+    call    scanf
+
+    # 2nd pstring
+    # get the length
+    movq    $format_int, %rdi    # pass the 1st argument for scanf
+    leaq    -272(%rbp), %rsi    # pass the 2nd argument for scanf
+    xorq    %rax, %rax
+    call    scanf
+    # get the string
+    movq    $format_str, %rdi    # pass the 1st argument for scanf
+    leaq    -271(%rbp), %rsi    # pass the 2nd argument for scanf
+    xorq    %rax, %rax
+    call    scanf
+
+    # get the users choice
+
+    movq    $format_int, %rdi   # pass the 1st argument for scanf
+    leaq    -16(%rbp), %rsi    # pass the 2nd argument for scanf
+    xorq    %rax, %rax
     call scanf
-    movq $format_str, %rdi # sending the format for the 1st string
-    leaq -511(%rbp), %rsi # loading the address to the string of the 1st pstring variable
-    xorq %rax, %rax # set %rax to 0
-    call scanf
-    movq $format_int, %rdi # sending the format for the size of the 2nd p string
-    leaq -512(%rbp), %rsi # loading the address to the size of the 2nd pstring variable
-    xorq %rax, %rax # set %rax to 0
-    call scanf
-    movq $format_str, %rdi # sending the format for the 2nd string
-    leaq -511(%rbp), %rsi # loading the address to the string of the 2nd pstring variable
-    xorq %rax, %rax # set %rax to 0
-    call scanf
+
+    # print
+
+    movq    $format_test1, %rdi
+    leaq    -528(%rbp), %rsi
+    leaq    -527(%rbp), %rcx
+    xorq    %rax, %rax
+    call printf
+
+    movq    $format_test2, %rdi
+    leaq    -272(%rbp), %rsi
+    leaq    -271(%rbp), %rcx
+    xorq    %rax, %rax
+    call printf
+
+    movq    $format_test3, %rdi
+    leaq    -16(%rbp), %rsi
+    xorq    %rax, %rax
+    call printf
+
+    popq    %r11
+    popq    %r12
+    popq    %rbp
+    movq    %rbp, %rsp
     ret
