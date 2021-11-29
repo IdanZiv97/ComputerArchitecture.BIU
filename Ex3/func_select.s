da29fd333d9dc9c218725a51231accc9354d0f1b
Idan Ziv <idanziv7@gmail.com>
2021-11-29 10:34:40 -0800

    Tested jump table access - works

--- Ex3/func_select.s
+++ Ex3/func_select.s
@@ -13,7 +13,7 @@
     .quad   .f_default #case 57 - default
     .quad   .f_default #case 58 - default
     .quad   .f_default #case 59 - default
-    .quad   .f_strlen #case 60
+    .quad   .f_pstrlen #case 60
 
     # literals for pstr.h functions
     msg_pstrlen:     .string "first pstring length: %d,second pstring length: %d\n"
@@ -26,40 +26,91 @@
     format_scan_char:    .string "%c"
     format_scan_string:    .string "%s"
 
+    # literal for jump table tests
+    test_pstrlen:    .string "pstrlen case has been invoked\n"
+    test_replaceChar:    .string "replaceChar case has been invoked\n"
+    test_pstrijcpy:    .string "pstrijcpy case has been invoked\n"
+    test_swapCase:    .string "swapCase case has been invoked\n"
+    test_pstrijcmp:    .string "pstrijcmp case has been invoked\n"
+    test_defaultCase:    .string "default case has been invoked\n"
+    
     .text
+    .global    main
+    .type   main, @function
+main:
+    movq    $50, %rdi # pass the user's input
+    call    run_func
+    ret
+
     .global run_func
     .type run_func, @function
 run_func:   # the case number is in %rdi (%edi), the 1st pString in %rsi, the 2nd pString in %rdx
     # correcting the input offset to match the first case
-    leaq -50(%rdi), %rbx # setting the choice to match the range
-    cmpq $10, %rbx # check if the choice is in range
-    ja .f_default # if the number is not in range we ha
-    jmp *.JUMP_TABLE(,%rsi,8)
+    leaq    -50(%rdi), %rbx # setting the choice to match the range
+    cmpq    $10, %rbx # check if the choice is in range
+    ja    .f_default # if the number is not in range we ha
+    jmp    *.JUMP_TABLE(,%rbx,8)
 
 .f_pstrlen:
-    # creating stack frame - saving the pString pointers
-    pushq    %rbp
-    movq    %rsp, %rbp
-    pushq    %rsi    # saving the 1st pString pointer
-    pushq    %rdx    # saving the 2nd pString poiner
+    movq    $test_pstrlen, %rdi # pass the proper format
+    xorq    %rax, %rax
+    call    printf
+    ret
+    /*
+    ; # creating stack frame - saving the pString pointers
+    ; pushq    %rbp
+    ; movq    %rsp, %rbp
+    ; pushq    %rsi    # saving the 1st pString pointer
+    ; pushq    %rdx    # saving the 2nd pString poiner
 
-    # getting the length of the 1st pString
-    leaq    -8(%rbp), %rdi    # send the pointer as the parameter
-    call    pstrlen
-    movq    %rax, %rsi    # saving the return value in %rsi
+    ; # getting the length of the 1st pString
+    ; leaq    -8(%rbp), %rdi    # send the pointer as the parameter
+    ; call    pstrlen
+    ; movq    %rax, %rsi    # saving the return value in %rsi
 
-    # getting the length of the 2nd pString
-    leaq    -16(%rbp), %rdi    # send the pointer as the parameter
-    call    pstrlen
-    movq    %rax, %rdx    # saving the return value in %rdi
+    ; # getting the length of the 2nd pString
+    ; leaq    -16(%rbp), %rdi    # send the pointer as the parameter
+    ; call    pstrlen
+    ; movq    %rax, %rdx    # saving the return value in %rdi
 
-    # print the message
-    movq    $msg_pstrlen, %rdi    # pasing the proper message to printf
-    call    printf    # Note: in %rsi and %rdx we saved the return values from calling pstrlen
+    ; # print the message
+    ; movq    $msg_pstrlen, %rdi    # pasing the proper message to printf
+    ; call    printf    # Note: in %rsi and %rdx we saved the return values from calling pstrlen
 
-    # resotre the stack frame
-    popq    %rdx
-    popq    %rsi
-    popq    %rbp
-    movq    %rbp, %rsp
+    ; # resotre the stack frame
+    ; popq    %rdx
+    ; popq    %rsi
+    ; popq    %rbp
+    ; movq    %rbp, %rsp
+    */
+    ret
+
+.f_replaceChar:
+    movq    $test_replaceChar, %rdi
+    xorq    %rax, %rax
+    call    printf
+    ret
+
+.f_pstrijcpy:
+    movq    $test_pstrijcpy, %rdi
+    xorq    %rax, %rax
+    call printf
+    ret
+
+.f_swapCase:
+    movq    $test_swapCase, %rdi
+    xorq    %rax, %rax
+    call    printf
+    ret
+
+.f_pstrijcmp:
+    movq    $test_pstrijcmp, %rdi
+    xorq    %rax, %rax
+    call    printf
+    ret
+
+.f_default:
+    movq    $test_defaultCase, %rdi
+    xorq    %rax, %rax
+    call printf
     ret
