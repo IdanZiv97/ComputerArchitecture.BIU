ae941ae411a0d42d8b002880039d9373f807a290
Idan Ziv <idanziv7@gmail.com>
2021-12-02 09:07:43 -0800

    finished pstrijcmp

--- Ex3/pstring.s
+++ Ex3/pstring.s
@@ -124,50 +124,46 @@ swapCase: # %rdi - *pstr
     .global pstrijcmp
     .type pstrijcmp, @function
 pstrijcmp: # %rdi - pstr1, %rsi - pstr2, %rdx - i, %rcx - j
-    # check for valid indexes
-    # test for i < pstr1.length
-    cmpb (%rdi), %dl
-    ja .pstrijcmp_invalidInput    # if not - invalid input
-    # test for j < pstr1.length
-    cmpb (%rdi),    %cl
-    ja .pstrijcmp_invalidInput    # if not - invalid input
-    # test for i < pstr2.length
-    cmpb (%rsi),    %dl
-    ja .pstrijcmp_invalidInput    # if not - invalid input
-    # test for j < pstr2.length
-    cmpb (%rsi),    %cl
-    ja .pstrijcmp_invalidInput    # if not - invalid input
-.pstrijcmp_loop:
-    # creating pointers to the right sections of each pstr
-    leaq (%rdi, %rdx),  %r8 # pstr1 + i
-    leaq (%rdi, %rcx),  %r9 # pstr1 + j
-    leaq (%rsi, %rdx),  %r10 # pstr2 + i
-    # comapre the pstr1[i] to pstr2[i]
-    movb    (%r8), %r11b    # can't access memory to memory 
-    cmpb    %r11b, (%r10)   
-    ja    .pstrijcmp_pstr2isBigger
-    jb    .pstrijcmp_pstr1isBigger
-    incq    %r8    # pstr1[i++]
-    incq    %r10    # pstr2[i++]
-    # check if the address of the pstr[i] != pstr[j]
-    cmpq    %r8, %r9
-    jle    .pstrijcmp_loop # haven't reached the end of the range
-    # In the case the ranges are equal
+    # check input
+    cmpl %edx, %ecx    # check if i > j
+    jl    .pstrijcmp_invalidInput
+    cmpb    $0, %dl    # check if i < 0
+    jl    .pstrijcmp_invalidInput
+    cmpb    (%rdi), %dl    # check if j > dst.len
+    jg    .pstrijcmp_invalidInput
+    cmpb    (%rsi), %cl    # check if j > src.len
+    jg    .pstrijcmp_invalidInput
+
+    # If input is valid, create pointers
+    incq    %rdi    # adjust pointer to the string part
+    incq    %rsi    # adjust pointer to the string part
+    movq    %rdi, %r11    # create a copy of the address, to reference the end of the range
+    leaq    (%rdi, %rdx), %rdi    # pointer to pstr1 + i
+    leaq    (%rsi, %rdx), %rsi    # pointer to pstr2 + i
+    leaq    (%r11, %rcx), %r11    # pointer to pstr1 + j
+    incq    %r11    # this points to pstr1 + j + 1 - we will use it as a sentry to finish the comparison
+.pstrijcmp_doWhile:
+    movb    (%rdi), %r11b    # pstr1[i] == pstr2[i] ?
+    cmpb    %r11b, (%rsi)    # can't access memory to memory
+    jg    .pstrijcmp_pstr1isBigger
+    jl    .pstrijcmp_pstr2isBigger
+    incq    %rdi    # pstr1++
+    incq    %rsi    # pstr2++
+    cmpq    %r11, %rdi    # check if we reached the end of the range
+    jne    .pstrijcmp_doWhile
     movq    $0, %rax
     ret
 
 .pstrijcmp_pstr1isBigger:
-    movq    $1, %rax
+    movl    $1, %eax    # set the compare result to 1
     ret
-
 .pstrijcmp_pstr2isBigger:
-    movq    $-1, %rax
+    movl    $-1, %eax     # set the compare result to -1
     ret
 
 .pstrijcmp_invalidInput:
-    pushq    %rdi
-    movq    $format_invalidInput, %rdi    # pass proper format for printf
+    movq    $format_invalidInput, %rdi    # pass the proper format for printf
     xorq    %rax, %rax    # set %rax to 0
     call    printf
-    popq    %rdi
+    movl    $-2, %eax     # return the proper compare result
     ret
