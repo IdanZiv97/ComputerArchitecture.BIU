32614ee458c634721cba517615f21ed94a0bfa3d
IdanZiv97 <idanziv7@gmail.com>
2022-01-10 13:59:58 +0200

    first try of blur with filter

--- Ex5/my solution/myfunction.c
+++ Ex5/my solution/myfunction.c
@@ -286,17 +286,17 @@ void blurWithNoFilter(Image *image) {
  * We can set commonly used variables to a register this way we assure fast handling and access to them.
  */
 
-void filterBlur(Image *image) {
+void blurWithFilter(Image *image) {
 	int imageSize = m * n;
 	int numOfPixels = imageSize + (imageSize << 1); // faster way to multiply by 3
 	register pixel *originalImage = (pixel *) image->data; //this way we refference 3 chars in one refernce instead of 1
 	register pixel *workingCopy = (pixel *) malloc(numOfPixels);
-	int minPixelIntensity = MAX_INTENSITY, maxPixelIntensity = MIN_INTENSITY;
-	pixel_sum minIntesity, maxIntensity, valuesSum;
-	pixel currentPixel;
-	//copy the image
 	memcpy(workingCopy, originalImage, numOfPixels);
+	pixel_sum valuesSum;
+	pixel currentPixel, minPixel, maxPixel;
+	//copy the image
 	register int row, column, lastIndex;
+	int currentIntensity;
 	lastIndex = m - 1; //the last index we can perfom applyKernel to, we know that m == n
 	/**
 	 * We will go over each pixel and created it convulation, we can calulate all the pixels in the square
@@ -304,21 +304,106 @@ void filterBlur(Image *image) {
 	 * Note: pixelij is kernel[i][j]
 	 */
 	register int firstRow, secondRow, thirdRow; // pixel rows inside the kernel
+	int maxPixelIntensity = MIN_INTENSITY, minPixelIntensity = MAX_INTENSITY;
+	int tempRow, tempCol;
 	for (row = 1; row < lastIndex; ++row) {
+		tempRow = MAX(row -1, 0); //for the first case when we start with M[1][1]
 		for (column = 1; column < lastIndex; ++column) {
-			firstRow = (row - 1) * m + column;
-			secondRow = row * m + column;
-			thirdRow = (row + 1) * m + column;
+			tempCol = MAX(column - 1, 0);
+			firstRow = tempRow * m + tempCol;
+			secondRow = (tempRow + 1) * m + tempCol; 
+			thirdRow = (tempRow + 2) * m + tempCol;
 			pixel pixel11 = workingCopy[firstRow];
+			currentIntensity = calcSum(pixel11.red, pixel11.green, pixel11.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel11;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel11;
+			}
 			pixel pixel12 = workingCopy[firstRow + 1];
+			currentIntensity = calcSum(pixel12.red, pixel12.green, pixel12.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel12;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel12;
+			}
 			pixel pixel13 = workingCopy[firstRow + 2];
+			currentIntensity = calcSum(pixel13.red, pixel13.green, pixel13.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel13;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel13;
+			}
 			pixel pixel21 = workingCopy[secondRow];
+			currentIntensity = calcSum(pixel21.red, pixel21.green, pixel21.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel21;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel21;
+			}
 			pixel pixel22 = workingCopy[secondRow + 1];
+			currentIntensity = calcSum(pixel22.red, pixel22.green, pixel22.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel22;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel22;
+			}
 			pixel pixel23 = workingCopy[secondRow + 2];
+			currentIntensity = calcSum(pixel23.red, pixel23.green, pixel23.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel23;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel23;
+			}
 			pixel pixel31 = workingCopy[thirdRow];
+			currentIntensity = calcSum(pixel31.red, pixel31.green, pixel31.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel31;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel31;
+			}
 			pixel pixel32 = workingCopy[thirdRow + 1];
+			currentIntensity = calcSum(pixel32.red, pixel32.green, pixel32.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel32;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel32;
+			}
 			pixel pixel33 = workingCopy[thirdRow + 2];
-
+			currentIntensity = calcSum(pixel33.red, pixel33.green, pixel33.blue);
+			if (currentIntensity <= minPixelIntensity) {
+				minPixelIntensity = currentIntensity;
+				minPixel = pixel33;
+			}
+			if (currentIntensity > maxPixelIntensity) {
+				maxPixelIntensity = currentIntensity;
+				maxPixel = pixel11;
+			}
+			
 			valuesSum.red = INT(pixel11.red) + INT(pixel12.red) + INT(pixel13.red) + INT(pixel21.red) + INT(pixel22.red)
 			+ INT(pixel23.red) + INT(pixel31.red) + INT(pixel32.red) + INT(pixel33.red);
 
@@ -328,12 +413,24 @@ void filterBlur(Image *image) {
 			valuesSum.blue = INT(pixel11.blue) + INT(pixel12.blue) + INT(pixel13.blue) + INT(pixel21.blue) + INT(pixel22.blue)
 			+ INT(pixel23.blue) + INT(pixel31.blue) + INT(pixel32.blue) + INT(pixel33.blue);
 
-			/**
-			 * add min max intensity refference 
-			 */
-			originalImage[secondRow + 1].red = (unsigned char) DIV_BY7(valuesSum.red);
-			originalImage[secondRow + 1].green = (unsigned char) DIV_BY7(valuesSum.green);
-			originalImage[secondRow + 1].blue = (unsigned char) DIV_BY7(valuesSum.blue);
+			// also remove the min and max pixels' values
+			valuesSum.red -= (INT(maxPixel.red) + INT(minPixel.red));
+			valuesSum.green -= (INT(maxPixel.green) + INT(minPixel.green));
+			valuesSum.blue -= (INT(maxPixel.blue) + INT(minPixel.blue));
+
+			//divide by 7
+			valuesSum.red = (valuesSum.red / 7);
+			valuesSum.green = (valuesSum.green / 7);
+			valuesSum.blue = (valuesSum.blue / 7);
+
+			currentPixel.red = (unsigned char) MIN(MAX(valuesSum.red, MIN_COLOR_VALUE), MAX_COLOR_VALUE);
+			currentPixel.green = (unsigned char) MIN(MAX(valuesSum.green, MIN_COLOR_VALUE), MAX_COLOR_VALUE);
+			currentPixel.blue = (unsigned char) MIN(MAX(valuesSum.blue, MIN_COLOR_VALUE), MAX_COLOR_VALUE);
+
+			originalImage[row * m + column] = currentPixel;
+			// reset the min max value for each iteratior
+			minPixelIntensity = MAX_INTENSITY;
+			maxPixelIntensity = MIN_INTENSITY;
 		}
 	}
 	free(workingCopy);
@@ -440,7 +537,7 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
 		writeBMP(image, srcImgpName, sharpRsltImgName);	
 	} else {
 		// apply extermum filtered kernel to blur image
-		doConvolution(image, 3, blurKernel, 7, true);
+		(image, 3, blurKernel, 7, true);
 
 		// write result image to file
 		writeBMP(image, srcImgpName, filteredBlurRsltImgName);
